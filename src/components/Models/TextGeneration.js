7a01e393396a980ed91a2fa37578d7d6f099e0de
horrordor <cccccc2002@outlook.com>
2024-08-29 15:19:09 +0800

    done

--- src/components/Models/TextGeneration.js
+++ src/components/Models/TextGeneration.js
@@ -12,7 +12,7 @@ const TextGeneration = () => {
 
     const handleButtonClick = () => {
         const messages = [
-            { role: 'system', content: 'You are a helpful, respectful and honest assistant. Your name is mio. Always answer as helpfully as possible, while being safe. Your answers should not include any harmful, unethical, racist, sexist, toxic, dangerous, or illegal content. Please ensure that your responses are socially unbiased and positive in nature. If a question does not make any sense, or is not factually coherent, explain why instead of answering something not correct. If you don\'t know the answer to a question, please don\'t share false information.' },
+            { role: 'system', content: 'You are a helpful, respectful and honest assistant. Your name is mio.' },
             { role: 'user', content: value }
         ];
         setLoading(true); // 在请求开始时，将loading设置为true
